6bd1e50fc4c32d966c6061eadfab4deda705a801
Sushant Bajracharya <bajra_sushant@gmail.com>
2023-11-06 10:09:19 +0545

    fixes

--- back/practice/index.js
+++ back/practice/index.js
@@ -35,10 +35,6 @@ let notes = [
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello world</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
