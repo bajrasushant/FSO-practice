45cf8fe74cdeb26abf55f26cfa6694e402689308
Sushant Bajracharya <bajra.sushant@gmail.com>
2024-04-02 07:50:39 +0545

    react routing

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 
 const Home = () => (
   <div>
@@ -21,43 +22,30 @@ const Users = () => (
 );
 
 const App = () => {
-  const [page, setPage] = useState("home");
-
-  const toPage = (page) => (event) => {
-    event.preventDefault();
-    setPage(page);
-  };
-
-  const content = () => {
-    if (page === "home") {
-      return <Home />;
-    } else if (page === "notes") {
-      return <Notes />;
-    } else if (page === "users") {
-      return <Users />;
-    }
-  };
-
   const padding = {
     padding: 5,
   };
 
   return (
-    <div>
+    <Router>
       <div>
-        <a href="" onClick={toPage('home')} style={padding}>
-        home
-        </a>
-        <a href="" onClick={toPage('notes')} style={padding}>
-        notes
-        </a>
-        <a href="" onClick={toPage('users')} style={padding}>
-        users
-        </a>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
       </div>
-      {content()}
-    </div>
-  )
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
